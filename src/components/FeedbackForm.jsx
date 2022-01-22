c7ba05be7d5e494e2096a5baec10a165218d1d69
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-01-22 23:08:02 +0700

    Real time form validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,27 +4,42 @@ import Button from './shared/Button'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
-  const handleTextChange = (e) => {
-    setText(e.target.value)
+  const handleTextChange = ({ target: { value } }) => {
+    if (value === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (value.trim().length < 10) {
+      setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true)
+    } else {
+      setMessage(null)
+      setBtnDisabled(false)
+    }
+
+    setText(value)
   }
 
   return (
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        {/* rating select component */}
+        {/* @todo - rating select component */}
         <div className='input-group'>
           <input
             onChange={handleTextChange}
             type='text'
-            value={text}
             placeholder='Write a review'
+            value={text}
           />
-          <Button type='submit' version='secondary'>
+          <Button type='submit' isDisabled={btnDisabled}>
             Send
           </Button>
         </div>
+
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
