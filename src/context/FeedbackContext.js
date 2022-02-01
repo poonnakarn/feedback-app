0887a04d81dcb26f2857930fb2629fbed8796dc8
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-02-01 22:00:01 +0700

    Connect to json server and add loading spinner

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,26 +1,31 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This item feedback item 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This item feedback item 2',
-      rating: 7,
-    },
-    {
-      id: 3,
-      text: 'This item feedback item 3',
-      rating: 9,
-    },
-  ])
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      'http://localhost:5001/feedback?_sort=id&_order=desc'
+    )
+    const data = await response.json()
+
+    setFeedback(data)
+
+    setIsLoading(false)
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm('Do you want to delete?')) {
@@ -34,12 +39,31 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([newFeedback, ...feedback])
   }
 
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    })
+  }
+
+  // update feedback item
+  const updateFeedback = (id, updItem) => {
+    // console.log(id, updItem, feedback)
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+    )
+  }
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
+        editFeedback,
+        updateFeedback,
       }}
     >
       {children}
