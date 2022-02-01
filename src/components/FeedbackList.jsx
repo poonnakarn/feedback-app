e8bf8a0b92237fb679ea8483ba70f07cabb585bb
Poonnakarn Panjasriprakarn <poonnakarn@gmail.com>
2022-02-01 15:04:33 +0700

    Pass feedback from useContext hook

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,10 @@
-import PropTypes from 'prop-types'
+import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
 import { motion, AnimatePresence } from 'framer-motion'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList({ handleDelete }) {
+  const { feedback } = useContext(FeedbackContext)
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
   }
@@ -34,16 +36,6 @@ function FeedbackList({ feedback, handleDelete }) {
   //     ))}
   //   </div>
   // )
-}
-
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  ).isRequired,
 }
 
 export default FeedbackList
